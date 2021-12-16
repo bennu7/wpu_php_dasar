4864eb8d00555b67c343daa3c6aa214ff86ff3d8
bennu7 <laluibnuhidayatullah@gmail.com>
2021-12-16 16:19:11 +0700

    duplikat pertemuan17

---
+++ pertemuan18/logout.php
@@ -0,0 +1,11 @@
+<?php
+// stop cookie
+setcookie("id", "", time() - 3600);
+setcookie("key", "", time() - 3600);
+
+session_start();
+$_SESSION = [];
+session_unset();
+session_destroy();
+header("Location: login.php");
+exit;
---
+++ pertemuan18/registrasi.php
@@ -0,0 +1,55 @@
+<?php
+require 'functions.php';
+
+if (isset($_POST['registrasi'])) {
+    if (registrasi($_POST) > 0) {
+        echo "<script>
+                alert('data user baru berhasil diregistrasi');
+                </script>";
+    } else {
+        mysqli_error($conn);
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrasi page</title>
+    <style>
+        label {
+            display: block;
+        }
+    </style>
+</head>
+
+<body>
+    <form action="" method="POST">
+        <ul>
+            <li>
+                <label for="username">Username : </label>
+                <input type="text" id="username" name="username">
+            </li>
+            <li>
+                <label for="password">password : </label>
+                <input type="password" id="password" name="password">
+            </li>
+            <li>
+                <label for="password2">konfigmasi password : </label>
+                <input type="password" id="password2" name="password2">
+            </li>
+            <li>
+                <button type="submit" name="registrasi">Register</button>
+            </li>
+
+        </ul>
+    </form>
+
+</body>
+
+</html>
