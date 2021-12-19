829a60eb879690e62afa11eaff7e68bb76d05c01
bennu7 <laluibnuhidayatullah@gmail.com>
2021-12-19 23:15:55 +0700

    duplikat pertemuan17 tanpa pagination

--- pertemuan19/index.php
+++ pertemuan19/index.php
@@ -7,19 +7,8 @@ if (!isset($_SESSION["login"])) {
 
 require 'functions.php';
 
-// membuat konfigurasi pagination
-$jumlahDataPerHalaman = 3;
-$jumlahData = count(query("SELECT * FROM mahasiswa"));
-$jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
 
-$halamanAktif = (isset($_GET["halaman"]))  ?  $_GET['halaman'] :  1;
-
-$awalData = ($jumlahDataPerHalaman * $halamanAktif) - $jumlahDataPerHalaman;
-
-var_dump($halamanAktif);
-
-
-$mahasiswa =  query("SELECT * FROM mahasiswa LIMIT $awalData, $jumlahDataPerHalaman");
+$mahasiswa =  query("SELECT * FROM mahasiswa");
 
 // mengurutkan berdasarkan data terbaru
 // $mahasiswa =  query("SELECT * FROM mahasiswa ORDER BY id DESC");
@@ -54,25 +43,6 @@ if (isset($_POST["cari"])) {
         <input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian..." autocomplete="off">
         <button type="submit" name="cari">Cari orang hilang</button>
     </form>
-
-    <br><br>
-
-    <?php if ($halamanAktif > 1) : ?>
-        <a href="?halaman=<?= $halamanAktif - 1; ?>"> &laquo; </a>
-    <?php endif; ?>
-
-    <!-- navigasi => gunakan perulangan -->
-    <?php for ($i = 1; $i <= $jumlahHalaman; $i++) : ?>
-        <?php if ($i == $halamanAktif) : ?>
-            <a href="?halaman=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?> </a>
-        <?php else : ?>
-            <a href="?halaman=<?= $i; ?>"><?= $i; ?> </a>
-        <?php endif; ?>
-    <?php endfor; ?>
-
-    <?php if ($halamanAktif < $jumlahHalaman) : ?>
-        <a href="?halaman=<?= $halamanAktif + 1; ?>"> &raquo; </a>
-    <?php endif; ?>
 
     <br>
     <table border="1" cellpadding="10" cellspacing="0">
